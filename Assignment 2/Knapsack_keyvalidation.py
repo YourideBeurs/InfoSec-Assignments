5c166ecc2f133f5fcbc36a26767d321a63b1fba7
youriWindesheim <57794097+youriWindesheim@users.noreply.github.com>
2023-09-29 15:20:21 +0200

    Implemented knapsack validation

--- Assignment 2/Knapsack_keyvalidation.py
+++ Assignment 2/Knapsack_keyvalidation.py
@@ -1,52 +1,106 @@
+import math
 
-def encrypt_message(inputNumber, publicKey):
-    cipherText = 0
-    for i in range(len(publicKey)):
-        if (inputNumber >> i) & 1:
-            cipherText += publicKey[i]
-    return cipherText
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
 
-def decrypt_message(cipherText, privateKey):
-    modular_inverse = pow(privateKey[-1], -1, privateKey[-2])
-
-    plaintext = 0
-
-    for i in range(len(privateKey) -1):
-        plaintext += (cipherText * modular_inverse * privateKey[i]) % privateKey[-2]
-    
-    return plaintext
+def convert_to_integer(key):
+    if is_integer(key):
+        return int(key)
+    else:
+        raise ValueError
 
 def validatePublicKey(n, m, publicKey, privateKey):
+    # Convert values to integer
+    try:
+        privateKey = [convert_to_integer(x) for x in privateKey]
+        publicKey = [convert_to_integer(x) for x in publicKey]
+    except ValueError:
+        return False
+
+    # Check if public key is not empty
+    if len(publicKey) == 0:
+        return False
+    
+    if len(publicKey) != len(privateKey):
+        return False
+    
+    # Check if public key consists of negative or 0 values
+    for key in publicKey:
+        if key <= 0:
+            return False
+    
     for i in range(len(privateKey)):
         result = n * privateKey[i]
         result = result % m
         if publicKey[i] != result:
-            return -1
-    return 1
+            return False
+    return True
 
 def validatePrivateKey(n, m, publicKey, privateKey):
-    inputNumber = 100
-    cipherText = encrypt_message(inputNumber, publicKey)
-    result = decrypt_message(cipherText, privateKey)
+    # Convert values to integer
+    try:
+        privateKey = [convert_to_integer(x) for x in privateKey]
+        publicKey = [convert_to_integer(x) for x in publicKey]
+    except ValueError:
+        return False
+    
+    # Check if private key is not empty
+    if len(privateKey) == 0:
+        return False
 
-    if inputNumber != result:
-        return -1
-    return 1
+    # Check if the sum of the private key is smaller than
+    if m <= sum(privateKey):
+        return False
+    
+    # Check if all values in private key are positive
+    for key in privateKey:
+        if key < 0:
+            return False
+
+    # Check for duplicates in the private key
+    seen = set()
+    for key in privateKey:
+        if key not in seen:
+            seen.add(key)
+        else:
+            return False
+
+    for key in privateKey:
+        if key > m:
+            return False
+
+    # Check if the private key is increasing
+    for i in range(len(privateKey)):
+        if privateKey[i] < sum(privateKey[:i]):
+            return False
+     
+    if math.gcd(m, n) > 1:
+        return False
+
+    return True
 
 mn = input()
-m, n = mn.split()[0], mn.split()[1]
+privateKey = input()
+publicKey = input()
+
+n, m = mn.split()[0], mn.split()[1]
 m = int(m)
 n = int(n)
 
-privateKey = input().split()
-publicKey = input().split()
-privateKey = [int(x) for x in privateKey]
-publicKey = [int(x) for x in publicKey]
+publicKey = publicKey.split()
+privateKey = privateKey.split()
 
 publicKeyValidation = validatePublicKey(n, m, publicKey, privateKey)
 privateKeyValidation = validatePrivateKey(n, m, publicKey, privateKey)
 
-if publicKeyValidation == -1 or privateKeyValidation == -1:
+if not privateKeyValidation:
     print(-1)
+elif not publicKeyValidation:
+    print(0)
 else:
     print(1)
